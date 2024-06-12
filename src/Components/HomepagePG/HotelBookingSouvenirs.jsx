9ff16829c52b36126d4539dc02c5e95f0d682fed
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-06-12 14:12:41 +0500

    HomePG done Alhamdulillah

--- src/Components/HomepagePG/HotelBookingSouvenirs.jsx
+++ src/Components/HomepagePG/HotelBookingSouvenirs.jsx
@@ -2,20 +2,32 @@ import styles from "../HomepagePG/HotelBookingSouvenirs.module.css";
 
 export default function HotelBookingSouvenirs() {
   return (
-    <div className={styles.divMain}>
-      <section className={styles.sectionSouvenirs}>
-        <div className={`${styles.divSouvenir} ${styles.divSouvenir1}`}>
-          <img src="../../../assets/HomepagePG/MonaLisa.jpg" />
-          <button>Buy</button>
-        </div>
-        <div className={`${styles.divSouvenir} ${styles.divSouvenir2}`}></div>
-        <div className={`${styles.divSouvenir} ${styles.divSouvenir3}`}></div>
-        <div className={`${styles.divSouvenir} ${styles.divSouvenir4}`}></div>
-      </section>
-      <section className={styles.sectionHotelBooking}>
-        <img src="../../../assets/HomepagePG/PalmHotel2.jpg" />
-        <button>Book A Hotel</button>
-      </section>
-    </div>
+    <main className={styles.mainBox}>
+      <h1>Buy souvenirs and book hotels</h1>
+      <div className={styles.divMain}>
+        <section className={styles.sectionSouvenirs}>
+          <div className={`${styles.divSouvenir} ${styles.divSouvenir1}`}>
+            <img src="../../../assets/HomepagePG/guitar.jpg" />
+            <button>Buy</button>
+          </div>
+          <div className={`${styles.divSouvenir} ${styles.divSouvenir2}`}>
+            <img src="../../../assets/HomepagePG/carpet.jpg" />
+            <button>Buy</button>
+          </div>
+          <div className={`${styles.divSouvenir} ${styles.divSouvenir3}`}>
+            <img src="../../../assets/HomepagePG/painting.jpg" />
+            <button>Buy</button>
+          </div>
+          <div className={`${styles.divSouvenir} ${styles.divSouvenir4}`}>
+            <img src="../../../assets/HomepagePG/oldCamera.jpg" />
+            <button>Buy</button>
+          </div>
+        </section>
+        <section className={styles.sectionHotelBooking}>
+          <img src="../../../assets/HomepagePG/PalmHotel2.jpg" />
+          <button>Book A Hotel</button>
+        </section>
+      </div>
+    </main>
   );
 }
--- src/Pages/HomePG.jsx
+++ src/Pages/HomePG.jsx
@@ -1,27 +1,36 @@
 import CitySliderCMP from "../Components/HomepagePG/CitySliderCMP";
+import HomePGfooter from "../Components/HomepagePG/HomePGfooter";
 import HomePGinfoCMP from "../Components/HomepagePG/HomePGinfoCMP";
 import HomepagePGheader from "../Components/HomepagePG/HomepagePGheader";
 import HotelBookingSouvenirs from "../Components/HomepagePG/HotelBookingSouvenirs";
 
 export default function HomePG() {
   return (
-    <div
+    <main
       style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "100px",
-        alignItems: "center",
-        width: "1000px ",
-        height: "2000px",
-        position: "absolute",
-        left: "50%",
-        transform: "translate(-50%)",
+        height: "2700px",
+        position: "relative",
+        marginTop: "70px",
       }}
     >
-      <HomepagePGheader />
-      <HomePGinfoCMP />
-      <CitySliderCMP />
-      <HotelBookingSouvenirs />
-    </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "100px",
+          alignItems: "center",
+          width: "1000px ",
+          position: "absolute",
+          left: "50%",
+          transform: "translate(-50%)",
+        }}
+      >
+        <HomepagePGheader />
+        <HomePGinfoCMP />
+        <CitySliderCMP />
+        <HotelBookingSouvenirs />
+      </div>
+      <HomePGfooter />
+    </main>
   );
 }
